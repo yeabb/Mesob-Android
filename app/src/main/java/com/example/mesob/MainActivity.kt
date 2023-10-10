d044950cede90d17db15d41b55cd734f718d1646
Yeabkal Biru <yeabkalbiru@gmail.com>
2023-10-10 15:49:02 -0400

    Added bottom nav menu and navigation drawer menu

--- app/src/main/java/com/example/mesob/MainActivity.kt
+++ app/src/main/java/com/example/mesob/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.mesob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.navigation.NavigationView
+import com.example.mesob.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tvNavUserName: TextView
+    private lateinit var tvNavEmail : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav )
+
+        val navigationView = findViewById<NavigationView>(R.id.navigationDrawer)
+        val headerView = navigationView.getHeaderView(0)
+        tvNavUserName = headerView.findViewById<TextView>(R.id.tvNavUserName)
+        tvNavEmail = headerView.findViewById<TextView>(R.id.tvNavEmail)
+
+
+
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
