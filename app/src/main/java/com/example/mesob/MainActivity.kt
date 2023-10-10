59c4dccf941d6f44c4f9480a8fa38a7f410545db
Yeabkal Biru <yeabkalbiru@gmail.com>
2023-10-10 18:33:54 -0400

    Fragment transaction done for bottom nav menu and nav drawer menu items

--- app/src/main/java/com/example/mesob/MainActivity.kt
+++ app/src/main/java/com/example/mesob/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.mesob
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.example.mesob.databinding.ActivityMainBinding
+import kotlin.collections.Map
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var tvNavUserName: TextView
     private lateinit var tvNavEmail : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,9 +39,49 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.miFoodMenu -> replaceFragment(FoodMenu())
+                R.id.miReservations -> replaceFragment(Reservations())
+                R.id.miRefer -> replaceFragment(Refer())
+//                R.id.miMap -> replaceFragment(kotlin.collections.Map())
+            }
+            true
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+
+        when(item.itemId){
+            R.id.miSettings -> replaceFragment(Settings())
+            R.id.miAbout -> replaceFragment(About())
+            R.id.miHelp -> replaceFragment(Help())
+            R.id.miShare -> {
+                val appLink = "https://play.google.com/store/apps/details?id=com.example.yourapp"
+
+                // Create an intent to share your app's link
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out this cool app: $appLink")
+
+                // Start the share activity
+                startActivity(Intent.createChooser(shareIntent, "Share via"))
+            }
+
+
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flFragment, fragment)
+        fragmentTransaction.commit()
     }
 }
